ecafe3a836724d34ca265ae37a6c3e47b33754b9
Eriko Mestrinari <mestrinari@gmail.com>
2023-06-16 23:44:53 -0300

    metronomo

--- Player.js
+++ Player.js
@@ -1,29 +1,14 @@
-import React , { useState } from 'react';
+import Metronome from "react-native-metronome";
 
-import { View, Text, TouchableOpacity } from 'react-native';
 
-const Player = () => {
-    const [isPlaying, setIsPlaying] = useState(false);
- 
-    
-    // Função para reproduzir ou pausar a música
-    const togglePlay = () => {
-      if (isPlaying) {
-        sound.pause();
-      } else {
-        sound.play();
-      }
-      setIsPlaying(!isPlaying);
-    };
-  
-    return (
-      <View>
-        <Text>Player de Música</Text>
-        <TouchableOpacity onPress={togglePlay}>
-{/*     <Text>{isPlaying ? 'Pausar' : 'Reproduzir'}</Text>  */}
-        </TouchableOpacity>
-      </View>
-  );
-};
+Metronome.play(80); //play with 80 bpm
+
+Metronome.nextSound(); //play next sound
+
+Metronome.stop(); // stop playing
+
+Metronome.isPlay(status => {
+  console.log(status);
+})
 
 export default Player;
